7c560e8d693aaa6525380ca38fbfa90039b976d2
VipulThapliyal <vipulthapliyal122@gmail.com>
2023-07-17 13:29:50 +0530

    Start with 212

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app_section_11/data/dummy_items.dart';
+import 'package:shopping_list_app_section_11/widgets/new_item.dart';
 
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({Key? key}) : super(key: key);
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => const NewItem())
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Groceries"),
+        actions: [
+           IconButton(
+            onPressed: _addItem,
+             icon: const Icon(Icons.add)
+           )
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
